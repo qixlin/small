0fd32ec519704a6a25597e26ea4619509d83be87
qixlin <qixlin@hotmail.com>
2018-07-20 12:51:58 +0800

    转62进制

---
+++ src/main/java/io/redos/small/model/ShortURL.java
@@ -0,0 +1,13 @@
+package io.redos.small.model;
+
+/**
+ * All rights Reserved, Powered By JIE
+ *
+ * @author JIE
+ * @version 1.0.0
+ * @date 2018/7/12 12:30
+ * @copyright ©2018
+ */
+public class ShortURL {
+    
+}
